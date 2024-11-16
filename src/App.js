db75f15807aec744ca0c77ae0617b03b0f91de23
somesh M <somavizhiyanmuniyappan@gmail.com>
2024-11-16 17:40:46 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Footer } from './components/footer/Footer'
+import { Footer1 } from './components/footer1/Footer1'
+import { Header } from './components/header/Header'
+import { Section1 } from './components/section1/Section1'
+import { Section2 } from './components/section2/Section2'
+import { Section3 } from './components/section3/Section3'
+import { Section4 } from './components/section4/Section4'
+import { Section6 } from './components/section6/Section6'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section6 />
+      <Footer />
+      <Footer1 />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
